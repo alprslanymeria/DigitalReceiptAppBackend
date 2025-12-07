cc878cfcf222218c19376ae07351a3498bbf5527
alparslan <alparslanorgnl@gmail.com>
2025-12-07 14:58:02 +0300

    Base infrastructure completed

---
+++ src/infastructure/App.Persistence/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App.Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    // DB SETS
+
+
+    // ON MODEL CREATING
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        // APPLY CONFIGURATIONS
+        // BU ASSEMBLY İÇERİSİNDE "IEntityTypeConfiguration" INTERFACE'İNİ IMPLEMENT EDEN TÜM CLASS'LARI OTOMATİK OLARAK BULUP UYGULAR
+        builder.ApplyConfigurationsFromAssembly(typeof(PersistenceAssembly).Assembly);
+
+        base.OnModelCreating(builder);
+    }
+}
